0878c1f699b28a06b2441c99687bfd89a71b78bc
Tanay Saxena <tanay.saxena002@gmail.com>
2020-01-08 19:00:23 +0530

    Java pro

---
+++ workspace_code/Core_Java/src/com/objectclass/HashCodeMeth.java
@@ -0,0 +1,22 @@
+package com.objectclass;
+
+public class HashCodeMeth
+{
+    int i;
+	 String j;
+	 
+	 public HashCodeMeth(int i,String j)
+	  {
+		   this.i=i;
+			this.j=j;
+	}
+public static void main(String []args)
+{
+	HashCodeMeth h=new HashCodeMeth(1,"abc");
+	HashCodeMeth h1=new HashCodeMeth(1,"abc");
+	int k=h.hashCode();
+	int k1=h1.hashCode();
+	System.out.println(k);
+	System.out.println(k1);
+}
+}
