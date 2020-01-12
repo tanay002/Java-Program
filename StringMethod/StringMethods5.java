4f9a414372db294656054780caa3e3ec8276adb9
Tanay Saxena <tanay.saxena002@gmail.com>
2020-01-13 01:37:37 +0530

    String methods

---
+++ StringMethod/StringMethods5.java
@@ -0,0 +1,16 @@
+package com.StringMethod;
+
+public class StringMethods5 
+{
+	public static void main(String[] args)
+	{
+   String s= String.join(" is ","Java","awsome");
+   System.out.println(s);
+   
+   String s1= String.join("/","05","03","1996");
+   System.out.println(s1);
+   
+   String s2=String.join(":","10","30","00");
+   System.out.println(s2);
+	}
+}
---
+++ StringMethod/StringMethods6.java
@@ -0,0 +1,20 @@
+package com.StringMethod;
+
+public class StringMethods6 
+{
+	public static void main(String[] args) 
+	{
+		String s="This is awsome concepts and java is wonderfull language";
+		int size=s.length();
+		System.out.println(size);
+		int ind=s.lastIndexOf('o');
+		System.out.println(ind);
+
+		int k=s.lastIndexOf("ava");
+		System.out.println(k);
+
+		System.out.println(s.lastIndexOf('a',35));  //lastIndexOf
+		System.out.println(s.lastIndexOf("ja",35));
+
+	}
+}
