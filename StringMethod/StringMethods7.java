60e302957aa5f2f34f9d25928b1ebcb9be679e41
Tanay Saxena <tanay.saxena002@gmail.com>
2020-01-13 02:11:21 +0530

    String methods

---
+++ StringMethod/StringMethods7.java
@@ -0,0 +1,23 @@
+package com.StringMethod;
+
+public class StringMethods7 
+{
+	public static void main(String[] args) 
+	{  
+		String s1="java is a very popular language and website to learn is tanayjava.com";  
+		String replaceString=s1.replace('a','e');
+		System.out.println(replaceString); 
+
+		String s="This is awsome concepts and java is wonderfull language";
+		int i=s.length();
+		System.out.println(i);
+
+		s=s.replace('i','j');
+		System.out.println(s);
+		s=s.replace("java","c");
+		System.out.println(s);
+
+           s1=s1.replaceAll("is","ok");
+           System.out.println(s1);
+	}
+}
